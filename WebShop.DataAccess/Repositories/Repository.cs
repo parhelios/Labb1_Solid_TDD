edc64692564fc6b8ec8982179593df2cc882220c
Tobias Lindberg <uet.lindberg@outlook.com>
2024-11-20 10:39:06 +0100

    [Labb1] Refactored Repository.cs

--- WebShop.DataAccess/Repositories/Repository.cs
+++ WebShop.DataAccess/Repositories/Repository.cs
@@ -8,29 +8,17 @@ public class Repository<TEntity>(MyDbContext context) : IRepository<TEntity>
 {
     private readonly DbSet<TEntity> _dbSet = context.Set<TEntity>();
 
-    public async Task<TEntity> GetByIdAsync(int id)
-    {
-        return await _dbSet.FindAsync(id);
-    }
+    public async Task<TEntity> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
 
-    public async Task<IEnumerable<TEntity>> GetAllAsync()
-    {
-        return await _dbSet.ToListAsync();
-    }
+    public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbSet.ToListAsync();
 
-    public async Task AddAsync(TEntity entity)
-    {
-        await _dbSet.AddAsync(entity);
-    }
+    public async Task AddAsync(TEntity entity) => await _dbSet.AddAsync(entity);
 
-    public async Task UpdateAsync(TEntity entity)
-    {
-        _dbSet.Update(entity);
-    }
+    public async Task UpdateAsync(TEntity entity) => _dbSet.Update(entity);
 
     public async Task DeleteAsync(int id)
     {
-        var entity = _dbSet.Find(id);
+        var entity = await _dbSet.FindAsync(id);
 
         if (entity is null)
             return;
@@ -38,8 +26,5 @@ public class Repository<TEntity>(MyDbContext context) : IRepository<TEntity>
         _dbSet.Remove(entity);
     }
 
-    public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
-    {
-        return await _dbSet.Where(predicate).ToListAsync();
-    }
+    public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate) => await _dbSet.Where(predicate).ToListAsync();
 }
