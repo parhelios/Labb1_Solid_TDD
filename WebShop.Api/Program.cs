8cd999f293f8e54001097759486909658eda53ca
Tobias Lindberg <uet.lindberg@outlook.com>
2024-11-27 11:17:13 +0100

    [Labb1] Swagger available outside of Development.

--- WebShop.Api/Program.cs
+++ WebShop.Api/Program.cs
@@ -43,11 +43,11 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
